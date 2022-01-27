a210ddb6b415abfd43c3ef566c2cae3617b51c26
Storm King <94218345+Master-Bizz@users.noreply.github.com>
2022-01-27 09:21:32 +0000

    fin

--- main.py
+++ main.py
@@ -97,3 +97,17 @@ print('ages' in student)
 print('ages' not in student)
 print('cheese' not in student)
 print('name' not in student)
+
+
+
+# clear()	      - Remove all items from dictionary
+# copy()	      - Returns a shallow copy of dictionary
+# fromkeys()	  - Returns a dictionary with the specified keys and value
+# get()	        - Returns the value of the specified key
+# items()	      - Returns a list containing a tuple for each key value pair
+# keys()	      - Returns a list containing the dictionary's keys
+# pop()	        - Removes the element with the specified key
+# popitem()	    - Removes the last inserted key-value pair
+# setdefault()	- Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	    - Updates the dictionary with the specified key-value pairs
+# values()	    - Returns a list of all the values in the dictionary
